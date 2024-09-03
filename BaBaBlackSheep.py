cfedb464b1b1f702af22310668907448b482ab86
Suganpreetham <148452036+Suganpreetham@users.noreply.github.com>
2024-09-03 22:54:46 +0530

    Add files via upload
    Codes of first 2 labs.

---
+++ BaBaBlackSheep.py
@@ -0,0 +1,5 @@
+S="Ba Ba Black Sheep"
+print(len(S))
+print("The first occurence of 'e' is at index: ",S.find("e"))
+print("Number of 'a' in string are: ",S.count("a"))
+print(S.replace("Ba Ba","Ta Ta"))
---
+++ Temperature.py
@@ -0,0 +1,3 @@
+celsius=float(input("enter temp in celcius:"))
+Fahrenheit = (celsius * 9/5) + 32
+print("temperature in fahrenheit is: ",Fahrenheit,"F")
